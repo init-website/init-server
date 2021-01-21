e84ee690f2c42e25daea7ce6399ce98b7eefbd66
Hyein Kim <kimhyein7110@gmail.com>
2021-01-21 22:34:30 +0900

    homework setting

--- init/mysite/models.py
+++ init/mysite/models.py
@@ -1,10 +1,23 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 
 # Create your models here.
 class Project(models.Model):
     title = models.CharField(max_length=200)
     pub_date = models.DateField('date published')
     body = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+class Homework(models.Model):
+    year = models.CharField(max_length=20)
+    title = models.CharField(max_length=200)
+    contents = models.TextField()
+    writer = models.CharField(max_length=20)
+    created_at  = models.DateTimeField(auto_now_add=True)
+    updated_at  = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
