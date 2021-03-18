5aaa1616ac0a0720332d69499d8e014ed7d530ab
Hyein Kim <kimhyein7110@gmail.com>
2021-03-18 20:49:16 +0900

    [homework/done] done to array complete

--- init/mysite/models.py
+++ init/mysite/models.py
@@ -1,3 +1,5 @@
+import os
+from uuid import uuid4
 from django.db import models
 from django.utils import timezone
 import datetime
@@ -14,11 +16,22 @@ class InitUser(AbstractUser):
     email = models.EmailField(blank=False, null=False, unique=True)
     year = models.CharField(max_length=20, null=False, blank=False)
     
+
+def profile_upload_to(instance, filename):
+  user = instance.user.username
+  # 확장자 추출
+  extension = os.path.splitext(filename)[-1].lower()
+  # 결합 후 return
+  return '/'.join([
+    'profile_uploads',
+    user + extension,
+  ])
+
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     nickname = models.CharField(max_length=10, null=True, blank=True)
     bio = models.TextField(null=True, blank=True)
-    img = ProcessedImageField(upload_to='profile_uploads/', null=True, blank=True, processors=[Thumbnail(200,200),], format='JPEG', options={'quality':90,})
+    img = ProcessedImageField(upload_to=profile_upload_to, null=True, blank=True, processors=[Thumbnail(200,200),], format='JPEG', options={'quality':90,})
     birthday = models.DateField(null=True, blank=True)
     git = models.URLField(max_length=60, null=True, blank=True)
 
